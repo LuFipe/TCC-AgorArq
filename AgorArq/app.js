2b3b2b125dd0f7b1cb2de60fb3fce4e4e836abb8
Luiz Felipe Castro Coelho <luizfelipecastrocoelho@gmail.com>
2022-03-30 13:26:45 -0300

    Layout Basico
    Criação do layout basico (template) com a view engine PUG
    Direcionamento para os arquivos estaticos
    Arquivos criados:
    -Index.pug
    -mainStyle.css

--- AgorArq/app.js
+++ AgorArq/app.js
@@ -1,9 +1,27 @@
+//IMPORTAÇÃO DAS BIBLIOTECAS FUNDAMENTAIS//
 const express = require('express');
+const path = require('path');
+const cookieParse = require('cookie-parser');
 
-const app = express();
+let app = express();
+
+//SETTANDO A VIEW ENGINE//
+app.set('views','/PUG');
+app.set('view engine','pug');
+
+//SETTANDO OS ARQUIVOS ESTATICOS COM ROTA MIDIA//
+app.use(express.static(path.join(__dirname,"front")));
+
+//CRIAÇÃO DA APLICAÇÃO//
 app.get('/',(req,res)=>{
-	res.send('Hello Wolrd');
+	res.render(path.join(__dirname,"PUG/index"));
 });
+
 let server = app.listen(3000,()=>{
 	console.log("Listenin in port 3000")
+});
+
+app.use((err, req, res, next)=>{
+	console.error(err.stack);
+	res.status(500).send('I fucked up');
 });
