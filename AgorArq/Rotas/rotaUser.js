10061c2b4398180025031196c431e8b86847ea68
Luiz Felipe Castro Coelho <luizfelipecastrocoelho@gmail.com>
2022-04-01 20:05:00 -0300

    Rotas
    Rotas e Paginas criadas:
        -BLOG
        -PROJETOS
        -ESCRITORIO
        -BANCO DE DADOS
        -SOBRE

--- AgorArq/Rotas/rotaUser.js
+++ AgorArq/Rotas/rotaUser.js
@@ -10,4 +10,16 @@ rota.get('/projetos',(req,res,next)=>{
 	res.render("projetos")
 })
 
+rota.get('/escritorio',(req,res,next)=>{
+	res.render("escritorio")
+})
+
+rota.get('/bd',(req,res,next)=>{
+	res.render("bd")
+})
+
+rota.get('/sobre',(req,res,next)=>{
+	res.render("sobre")
+})
+
 module.exports = rota;
