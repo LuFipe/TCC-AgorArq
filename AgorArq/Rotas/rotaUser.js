97b69da9066a535a634830a261ebef4e4ce60d5e
Luiz Felipe Castro Coelho <luizfelipecastrocoelho@gmail.com>
2022-04-22 17:35:44 -0300

    PROJETOS
    Criação do layout basico e definição de projetos
            -PROJETOS
            -TRABALHOS
            -ARTIGOS

--- AgorArq/Rotas/rotaUser.js
+++ AgorArq/Rotas/rotaUser.js
@@ -30,4 +30,48 @@ rota.get('/tcc',(req,res)=>{
 	res.render("tcc")
 })
 
+rota.get('/paisagismo',(req,res)=>{
+	res.render("paisagismo")
+})
+
+rota.get('/cultural',(req,res)=>{
+	res.render("cultural")
+})
+
+rota.get('/edificil',(req,res)=>{
+	res.render("edificil")
+})
+
+rota.get('/hospital',(req,res)=>{
+	res.render("hospital")
+})
+
+rota.get('/loteamento',(req,res)=>{
+	res.render("loteamento")
+})
+
+rota.get('/catalogo',(req,res)=>{
+	res.render("catalogo")
+})
+
+rota.get('/washington',(req,res)=>{
+	res.render("washington")
+})
+
+rota.get('/permacultura',(req,res)=>{
+	res.render("permacultura")
+})
+
+rota.get('/estudo_paisagismo',(req,res)=>{
+	res.render("estudo_paisagismo")
+})
+
+rota.get('/diag_praca',(req,res)=>{
+	res.render("diag_praca")
+})
+
+rota.get('/diag_urb',(req,res)=>{
+	res.render("diag_urb")
+})
+
 module.exports = rota;
